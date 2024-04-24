15db52712addef41aefdbf45531c0668b13629e7
IllTamer <mail@illtamer.com>
2024-04-24 18:10:02 +0800

    feat: update pkgs

---
+++ pkg/draw/path.go
@@ -0,0 +1,10 @@
+package draw
+
+const (
+	Textures       = "/assets/minecraft/textures"
+	ItemTextures   = Textures + "/item"
+	GUITextures    = Textures + "/gui"
+	EntityTextures = Textures + "/entity"
+)
+
+// entity: 2d to 3d
---
+++ pkg/url/mojang.go
@@ -0,0 +1,8 @@
+package url
+
+// 1.7.2 +
+// https://wiki.vg/Zh:Game_files
+
+const (
+	VersionManifest = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+)
