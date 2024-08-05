32443b16fc6d92b86eded5e676c1abe09dcc2acb
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-08-05 17:10:49 +0800

    feat: add icda-versions api

--- internal/loader/init.go
+++ internal/loader/init.go
@@ -1,4 +1,24 @@
 package loader
 
+import (
+	"github.com/IUnlimit/minecraft-view-generator/pkg/url"
+	"github.com/IUnlimit/minecraft-view-generator/tools"
+	"github.com/buger/jsonparser"
+	log "github.com/sirupsen/logrus"
+)
+
 func Init() {
+	loadSupportVersions()
+}
+
+func loadSupportVersions() {
+	bytes, err := tools.Get(url.ICDAVersions)
+	if err != nil {
+		log.Fatalf("Can't get support versions: %v", err)
+	}
+	log.Debugf("Load support versions: %s", string(bytes))
+
+	jsonparser.ArrayEach(bytes, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		log.Info(jsonparser.GetString(value))
+	}, "versions")
 }
