39b0f8d501954149f5d52af2bc0f46742e72607f
IllTamer <mail@illtamer.com>
2024-08-11 23:27:08 +0800

    feat: add skin pkg

--- pkg/draw/skin.go
+++ pkg/draw/skin.go
@@ -1,4 +1,33 @@
 package draw
 
-// 2D 皮肤解析
-// go get github.com/mineatar-io/skin-render
+import (
+	"github.com/IUnlimit/minecraft-view-generator/internal/tools"
+	"github.com/mineatar-io/skin-render"
+	"image"
+	"image/draw"
+)
+
+type Skin struct {
+	File *image.NRGBA
+}
+
+func NewSkin(filePath string) (*Skin, error) {
+	file, err := tools.ReadImage(filePath)
+	if err != nil {
+		return nil, err
+	}
+	playerSkin := image.NewNRGBA(file.Bounds())
+	draw.Draw(playerSkin, file.Bounds(), file, image.Pt(0, 0), draw.Src)
+	return &Skin{
+		File: playerSkin,
+	}, nil
+}
+
+// GetFace 1 scale for 8 px
+func (s *Skin) GetFace() *image.NRGBA {
+	return skin.RenderFace(s.File, skin.Options{
+		Scale:   2,
+		Overlay: true,
+		Slim:    true,
+	})
+}
---
+++ pkg/draw/skin_test.go
@@ -0,0 +1,15 @@
+package draw
+
+import (
+	"github.com/fogleman/gg"
+	"testing"
+)
+
+func TestSkin(t *testing.T) {
+	skin, err := NewSkin("ori-skin.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	face := skin.GetFace()
+	_ = gg.SavePNG("./output/face.png", face)
+}
